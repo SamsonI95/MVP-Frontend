fc65e5844a6e0ae5162beda140b000ab7935b88a
phantomcancode <vodumeh@gmail.com>
2024-06-17 10:05:31 +0100

    fixing bugs on the admin dashboard

--- src/Data/formikUtils.js
+++ src/Data/formikUtils.js
@@ -3,10 +3,10 @@ import * as Yup from "yup";
 
 axios.defaults.baseURL = `https://mvp-backend-bzvi.onrender.com/v1`;
 
-const passwordRegExp =
-  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#;:])[A-Za-z\d@$!%*?&#;:]{8,}$/;
+// const passwordRegExp =
+//   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#;:])[A-Za-z\d@$!%*?&#;:]{6,12}$/;
 
-const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// const emailRegExp = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
 // const codeRegex = /^[0-9]*$/
 
@@ -18,22 +18,22 @@ export const signIn = Yup.object({
     .required("required"),
 });
 
-export const signUp = Yup.object({
-  firstName: Yup.string().required("required"),
-  lastName: Yup.string().required("required"),
-  orgName: Yup.string().required("required"),
-  email: Yup.string()
-    .email("Enter Your E-mail Address")
-    .matches(emailRegExp, "Invalid E-mail Address")
-    .required("required"),
-  password: Yup.string()
-    .max(8, "Password must not exceed 8 characters!")
-    .matches(passwordRegExp, "Invalid Password Input")
-    .required("required"),
-  password: Yup.string()
-    .oneOf([Yup.ref("password")], "Password must match!")
-    .required("required"),
-});
+// export const signUp = Yup.object({
+//   firstName: Yup.string().required("required"),
+//   lastName: Yup.string().required("required"),
+//   orgName: Yup.string().required("required"),
+//   email: Yup.string()
+//     .email("Enter Your E-mail Address")
+//     .matches(emailRegExp, "Invalid E-mail Address")
+//     .required("required"),
+//   password: Yup.string()
+//     .max(8, "Password must not exceed 8 characters!")
+//     .matches(passwordRegExp, "Invalid Password Input")
+//     .required("required"),
+//   password: Yup.string()
+//     .oneOf([Yup.ref("password")], "Password must match!")
+//     .required("required"),
+// });
 
 export const verifyEmail = Yup.object({
   code: Yup.string().required("required"),
