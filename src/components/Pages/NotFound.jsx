e00798cf3a71fe2694ede80a872373ed9da12df4
phantomcancode <vodumeh@gmail.com>
2024-06-20 08:55:14 +0100

    added react toastify for action notifications

--- src/components/Pages/NotFound.jsx
+++ src/components/Pages/NotFound.jsx
@@ -26,7 +26,7 @@ const NotFound = () => {
             <p className='font-black text-[#2F4EED] text-[1.65rem]'>404</p>
             <h1 className='font-bold uppercase text-[2rem] md:text-[3rem] w-full md:w-[80%]'>The page you&apos;re looking for does not exist. sorry.</h1>
         </div>
-        <Link className='bg-[#2F4EED] px-4 py-2 rounded-lg text-white h-[3.5em] gap-[40px] font-semibold flex justify-center items-center cursor-pointer' onClick={handleNavigate}>Go Back Home</Link>
+        <Link className='bg-[#2F4EED] shadow-customInset hover:bg-buttonPrimaryHover px-4 py-2 rounded-lg text-white h-[3.5em] gap-[40px] font-semibold flex justify-center items-center cursor-pointer' onClick={handleNavigate}>Go Back Home</Link>
     </section>
   )
 }
