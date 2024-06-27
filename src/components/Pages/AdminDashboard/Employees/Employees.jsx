8839d7aa973a5b098d4b47a224b49263b62a2253
SamsonI95 <samsoniweibo@gmail.com>
2024-06-27 18:07:37 +0100

    Employee update

--- src/components/Pages/AdminDashboard/Employees/Employees.jsx
+++ src/components/Pages/AdminDashboard/Employees/Employees.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { FiArrowLeft, FiSearch } from "react-icons/fi";
 import EmployeeTable from "../../../Page Components/EmployeeTable";
-import AddEmployees from "../../../Page Components/Modals/AddEmployee";
+import Add from "../../../Page Components/Modals/Add";
 
 const Employees = () => {
   const [searchClick, setSearchClick] = useState(false);
@@ -99,7 +99,7 @@ const Employees = () => {
 
       {/* Add Employee Modal */}
       {addEmployees ? (
-        <AddEmployees
+        <Add
           addEmployees={addEmployees}
           setAddEmployees={setAddEmployees}
         />
