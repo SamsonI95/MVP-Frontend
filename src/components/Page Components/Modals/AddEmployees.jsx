d91edb946f2b03796fd165cc0d190e281603c04f
phantomcancode <vodumeh@gmail.com>
2024-06-27 15:23:56 +0100

    employee modal

--- src/components/Page Components/Modals/AddEmployees.jsx
+++ src/components/Page Components/Modals/AddEmployees.jsx
@@ -8,8 +8,8 @@ const AddEmployees = ({addEmployees, setAddEmployees}) => {
 
     const [assets, setAssets] = useState(false);
   return (
-    <div className={addEmployees ? `w-full h-screen modalBg fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-[999] duration-300` : `w-full h-screen modalBg fixed top-0 left-0 right-[-200%] bottom-0 flex justify-center items-center z-[999] duration-300`}>
-        <aside className={`absolute bg-white w-[450px] h-full flex flex-col justify-center items-start duration-300 ${addEmployees ? `right-0 top-0` : `right-[-200%] top-0`}`}>
+    <div className={addEmployees ? `w-full h-full modalBg fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center z-[999] duration-300` : `w-full h-full modalBg fixed top-0 left-0 right-[-200%] bottom-0 flex justify-center items-center z-[999] duration-300`}>
+        <div className={`absolute bg-white w-[450px] h-[1024px] flex flex-col justify-center items-start duration-300 ${addEmployees ? `right-0 top-0` : `right-[-200%] top-0`}`}>
             <div className='flex justify-end items-center gap-[152px] py-[18px] px-[24px] w-full'>
             <FaXmark onClick={() => setAddEmployees(false)} className='cursor-pointer text-[1.125rem] text-[#151515] font-bold' />
             </div>
@@ -17,8 +17,8 @@ const AddEmployees = ({addEmployees, setAddEmployees}) => {
                 <h1 className='text-[#151515] text-[1.75rem] font-bold leading-9 tracking-[-.56px]'>Add employee</h1>
                 <p className='text-[1rem] text-[#9C9C9C] font-semibold leading-6'>Enter employee details</p>
             </div>
-            <form className='w-full h-full' action="">
-                <div className='w-full h-[748px] flex flex-col items-start gap-8 p-6'>
+            <form className='w-full h-[748px] flex flex-col items-start gap-8 p-6' action="">
+                {/* <div className='w-full h-[748px] flex flex-col items-start gap-8 p-6'> */}
                     <div className='flex flex-col items-start gap-2 w-full'>
                         <label className='text-[#151515] text-[.875rem] font-semibold leading-4' htmlFor="First Name">First Name</label>
                         <input className='outline-none border-none bg-[#F7F7F7] h-[56px] px-4 w-full self-stretch flex items-center text-base placeholder:text-[#838385] font-normal leading-6 text-[#000000] rounded-lg' type="text" placeholder='Enter first name' />
@@ -61,12 +61,12 @@ const AddEmployees = ({addEmployees, setAddEmployees}) => {
                             <IoIosArrowForward className='text-[#1F2937] text-[1.8rem] cursor-pointer font-bold' />
                         </div>
                     </div>
-                </div>
+                {/* </div> */}
                 <div className='pt-4 pb-10 px-6 flex w-full flex-col items-start gap-[10px]'>
                     <button className='h-[56px] self-stretch flex justify-center items-center px-2 bg-[#2F4EED] rounded-lg text-base text-white font-semibold leading-[18px]'>Add</button>
                 </div>
             </form>
-        </aside>
+        </div>
     </div>
   )
 }
