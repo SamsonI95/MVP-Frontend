df0f7822030aab12c7a35a02ed4fe273cfb5d3f1
phantomcancode <vodumeh@gmail.com>
2024-07-04 16:28:38 +0100

    landing page fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  darkMode: ["class"],
-  content: [
-    './pages/**/*.{js,jsx}',
-    './components/**/*.{js,jsx}',
-    './app/**/*.{js,jsx}',
-    './src/**/*.{js,jsx}',
-  ],
-  prefix: "",
+export default {
+  darkMode: "selector",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
+    screens: {
+      // {'max': '1535px'}
+      "sm": "320px",
+      "md": "768px",
+      "lg": "1360px",
     },
     extend: {
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+      colors: {
+        "textSecondary": "#6E6E6E",
+       "buttonPrimary": "#2F4EED",
+       "buttonPrimaryHover": "#3A1DA0",
+        "landingInput": "#F7F7F7",
+        "landingInputText": "#838385",
+        "landingCardBg": "#151515"
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      "boxShadow": {
+        "customInset": "0px -4px 0px 0px rgba(0, 0, 0, 0.25) inset",
+      }, 
     },
   },
-  plugins: [require("tailwindcss-animate")],
-}
+  plugins: [],
+};
