00624e46cc12213fb6d34664d112f3b21e398812
himarnoel <olaimarnoel>
2024-07-04 16:16:28 +0100

    issue resolved

--- src/components/Pages/AdminDashboard/Employees/Employees.jsx
+++ src/components/Pages/AdminDashboard/Employees/Employees.jsx
@@ -42,81 +42,48 @@ const Employees = () => {
                 </div>
                 <button onClick={() => setAddEmployees(true)} className='flex justify-center items-center bg-[#2F4EED] rounded-[50px] px-2 w-[129px] h-[40px] text-white leading-4 text-[.875rem] font-semibold'>Add Employee</button>
               </div>
-              <button
-                onClick={() => setAddEmployees(true)}
-                className="flex justify-center items-center bg-[#2F4EED] rounded-[50px] px-2 w-[129px] h-[40px] text-white leading-4 text-[.875rem] font-semibold"
-              >
-                Add Employee
-              </button>
-            </div>
-          ) : (
-            <div className="flex gap-2 px-[16px] py-[12px] w-full justify-end items-center md:hidden">
-              <div
-                onClick={() => setSearchClick(true)}
-                className="flex md:hidden cursor-pointer justify-between items-center md:w-[350px] h-[40px] px-2 md:px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6"
-              >
-                <FiSearch className="text-[#1F2937] text-[1.25rem]" />
+            ) : (
+              <div className='flex gap-2 px-[16px] py-[12px] w-full justify-end items-center md:hidden'>
+                <div onClick={() => setSearchClick(true)} className='flex md:hidden cursor-pointer justify-between items-center md:w-[350px] h-[40px] px-2 md:px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6'>
+                  <FiSearch className='text-[#1F2937] text-[1.25rem]' />
+                </div>
+                <button className='flex md:hidden justify-center items-center bg-[#2F4EED] rounded-[50px] px-2 w-[129px] h-[40px] text-white leading-4 text-[.875rem] font-semibold'>Add Employee</button>
               </div>
-              <button className="flex md:hidden justify-center items-center bg-[#2F4EED] rounded-[50px] px-2 w-[129px] h-[40px] text-white leading-4 text-[.875rem] font-semibold">
-                Add Employee
-              </button>
-            </div>
-          )}
+            )
+          }
 
-          <div className="hidden md:flex justify-between items-center w-[350px] h-[40px] px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6">
-            <FiSearch className="text-[#1F2937] text-[1.25rem]" />
-            <input
-              className="hidden md:flex w-[350px] flex-col items-start bg-transparent outline-none border-none"
-              type="text"
-              placeholder="Place"
-            />
+          <div className='hidden md:flex justify-between items-center w-[350px] h-[40px] px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6'>
+            <FiSearch className='text-[#1F2937] text-[1.25rem]' />
+            <input className='hidden md:flex w-[350px] flex-col items-start bg-transparent outline-none border-none' type="text" placeholder='Place' />
           </div>
 
-          {searchClick === false ? (
-            <div className="hidden w-full">
-              <div className="flex justify-end items-center gap-4 px-[16px] py-[12px] w-full">
-                <FiArrowLeft
-                  onClick={() => setSearchClick(false)}
-                  className="cursor-pointer text-[#1F2937] text-[1.25rem]"
-                />
-                <div className="flex md:hidden justify-between items-center w-[350px] h-[40px] px-2 md:px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6">
-                  <input
-                    className="hidden md:flex w-[350px] flex-col items-start bg-transparent outline-none border-none"
-                    type="text"
-                    placeholder="Place"
-                  />
+          {
+            searchClick === false ? (
+              <div className='hidden w-full'>
+                <div className='flex justify-end items-center gap-4 px-[16px] py-[12px] w-full'>
+                  <FiArrowLeft onClick={() => setSearchClick(false)} className='cursor-pointer text-[#1F2937] text-[1.25rem]' />
+                  <div className='flex md:hidden justify-between items-center w-[350px] h-[40px] px-2 md:px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6'>
+                    <input className='hidden md:flex w-[350px] flex-col items-start bg-transparent outline-none border-none' type="text" placeholder='Place' />
+                  </div>
                 </div>
               </div>
-            </div>
-          ) : (
-            <div className="flex md:hidden w-full">
-              <div className="flex justify-end items-center gap-4 px-[16px] py-[12px] w-full">
-                <FiArrowLeft
-                  onClick={() => setSearchClick(false)}
-                  className="cursor-pointer text-[#1F2937] text-[1.25rem]"
-                />
-                <div className="flex md:hidden justify-between items-center w-[295px] h-[40px] px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6">
-                  <input
-                    className="flex w-[295px] flex-col items-start bg-transparent outline-none border-none"
-                    type="text"
-                    placeholder="Place"
-                  />
+            ) : (
+              <div className='flex md:hidden w-full'>
+                <div className='flex justify-end items-center gap-4 px-[16px] py-[12px] w-full'>
+                  <FiArrowLeft onClick={() => setSearchClick(false)} className='cursor-pointer text-[#1F2937] text-[1.25rem]' />
+                  <div className='flex md:hidden justify-between items-center w-[295px] h-[40px] px-[16px] self-stretch gap-2 rounded-[50px] bg-[#F7F7F7] text-[#000000] placeholder:text-[#838385] text-[1rem] font-normal leading-6'>
+                    <input className='flex w-[295px] flex-col items-start bg-transparent outline-none border-none' type="text" placeholder='Place' />
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
+            )
+          }
 
-          <button
-            onClick={() => setAddEmployees(true)}
-            className="hidden md:flex justify-center items-center bg-[#2F4EED] rounded-[50px] px-2 w-[129px] h-[40px] text-white leading-4 text-[.875rem] font-semibold"
-          >
-            Add Employee
-          </button>
+          <button onClick={() => setAddEmployees(true)} className='hidden md:flex justify-center items-center bg-[#2F4EED] rounded-[50px] px-2 w-[129px] h-[40px] text-white leading-4 text-[.875rem] font-semibold'>Add Employee</button>
         </div>
       </div>
 
       {/* EMployee Table */}
-
       <EmployeeTable existData={existData} setExistData={setExistData} loadEmployees={loadEmployees} setLoadEmployees={setLoadEmployees} eachEmployee={eachEmployee} setEachEmployee={setEachEmployee} updateEmployees={updateEmployees} setUpdateEmployees={setUpdateEmployees} />
 
       {/* Add Employee Modal */}
@@ -134,7 +101,7 @@ const Employees = () => {
         updateEmployees ? (<UpdateDeleteEmployeeModal existData={existData} setExistData={setExistData} loadEmployees={loadEmployees} setLoadEmployees={setLoadEmployees} eachEmployee={eachEmployee} setEachEmployee={setEachEmployee} updateEmployees={updateEmployees} setUpdateEmployees={setUpdateEmployees} schEmployees={schEmployees} setSchEmployees={setSchEmployees} />) : ``
       }
     </section>
-  );
-};
+  )
+}
 
-export default Employees;
+export default Employees
