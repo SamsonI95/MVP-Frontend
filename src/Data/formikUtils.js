b02038a1808c69b09800a439634c3d5ffd85439d
himarnoel <olaimarnoel>
2024-07-18 22:50:50 +0100

    formik utils

--- src/Data/formikUtils.js
+++ src/Data/formikUtils.js
@@ -107,13 +107,13 @@ export const truncateWalletAddress = (address) => {
 export const formatNumber = (numberString, numfixed = 2) => {
   // Check if the input is a number
   if (typeof numberString !== "number") {
-    return;
+    return 0.00;
   }
   // Parse the number to float
   const number = parseFloat(numberString);
 
   // Format with commas and two decimal places
-  return number.toFixed(numfixed).replace(/\d(?=(\d{3})+\.)/g, "$&,");
+  return number.toFixed(numfixed).replace(/\d(?=(\d{3})+\.)/g, "$&,") ?? 0.00;
 };
 
 export const capitalizeFirstLetterOfEachWord = (inputString) => {
